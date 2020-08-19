1432304d065efc7259cfac359e95cb8ecb878ee3
lozito <lozolmao@gmail.com>
2020-08-19 13:27:27 -0300

    inital commit

---
+++ src/main/java/Aiden.java
@@ -0,0 +1,26 @@
+import listener.MessageListener;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.ReadyEvent;
+
+import javax.security.auth.login.LoginException;
+
+public class Aiden {
+
+    public static void main(String[] args) throws LoginException, InterruptedException {
+
+        JDA aiden = JDABuilder.createDefault("NzQ1NjQzMDM0NDE0MTUzODkz.Xz0wMA.syPSPNr2Cc83Us_il-o-Yh8ZxQY").build();
+        aiden.awaitReady();
+        aiden.addEventListener(new MessageListener());
+        aiden.getPresence().setActivity(Activity.streaming("sentimientos", "https://twitch.tv/EasterEggs"));
+    }
+
+    public void onReady(GenericEvent event) {
+        if (event instanceof ReadyEvent) {
+            System.out.println("API is ready!");
+        }
+    }
+
+}
---
+++ src/main/java/listener/MessageListener.java
@@ -0,0 +1,16 @@
+package listener;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MessageListener extends ListenerAdapter {
+
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        String message = event.getMessage().getContentRaw();
+        String name = event.getMember().getUser().getName();
+        if (message.contains("hola")) {
+            event.getChannel().sendMessage("Hola "+ name + "!").queue();
+        }
+    }
+
+}
